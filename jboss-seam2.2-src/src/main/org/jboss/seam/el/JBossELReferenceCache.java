bcdc80dc1899d79528653b721d3e02683398d23a
heyoulin <heyoulin@hotmail.com>
2010-11-15 00:24:06 +0800

    support jboss-el 2.0,Fix phonebook commandlink problem

--- jboss-seam2.2-src/src/main/org/jboss/seam/el/JBossELReferenceCache.java
+++ jboss-seam2.2-src/src/main/org/jboss/seam/el/JBossELReferenceCache.java
@@ -4,22 +4,31 @@ import org.jboss.el.util.ReflectionUtil;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.Destroy;
+import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Startup;
-
+import static org.jboss.seam.annotations.Install.BUILT_IN;
 
 @Name("org.jboss.seam.el.referenceCache")
 @Scope(ScopeType.APPLICATION)
+@Install(precedence = BUILT_IN, classDependencies="org.jboss.el.util.ReflectionUtil")
 @Startup
 public class JBossELReferenceCache {
+    private int version=1;
     @Create
     public void start() {
-        ReflectionUtil.startup();
+       try{
+          ReflectionUtil.startup();
+       }catch(NoSuchMethodError e)
+       {
+          version=2;
+       }
     }
     
     @Destroy 
     public void stop() {
-        ReflectionUtil.shutdown();
+       if(version==1)
+          ReflectionUtil.shutdown();
     }
 }
